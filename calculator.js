1092b642114349c1aa639406c1387cf83a0f4e6f
Tony Maluki <tonymaluki@gmail.com>
2021-12-29 13:03:28 +0300

    change to qeryselector

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
 function myCalc() {
-  let first = document.getElementById("first").value;
-  let second = document.getElementById("second").value;
+  let first = document.querySelector("#first").value;
+  let second = document.querySelector("#second").value;
   let sum = Number(first) + Number(second);
-  document.getElementById("answerHTML").value = sum;
+  document.querySelector("#answerHTML").value = sum;
 }
