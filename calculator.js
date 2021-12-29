abdad7b033bf6f53778c97c6d1cb542c41d83073
Tony Maluki <tonymaluki@gmail.com>
2021-12-29 14:24:10 +0300

    subtraction functionality

--- calculator.js
+++ calculator.js
@@ -1,6 +1,13 @@
-function myCalc() {
+function add() {
   let first = document.querySelector("#first").value;
   let second = document.querySelector("#second").value;
   let sum = Number(first) + Number(second);
+  document.querySelector("#answerHTML").value = sum;
+}
+
+function subtract() {
+  let first = document.querySelector("#first").value;
+  let second = document.querySelector("#second").value;
+  let sum = Number(first) - Number(second);
   document.querySelector("#answerHTML").value = sum;
 }
