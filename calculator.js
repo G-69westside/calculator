d9e78c94d9886ed5ccbfe2ecb5609914e6593b44
Tony Maluki <tonymaluki@gmail.com>
2021-12-30 09:59:28 +0300

    modulus operator

--- calculator.js
+++ calculator.js
@@ -25,3 +25,9 @@ function divide() {
   let sum = Number(first) / Number(second);
   document.querySelector("#answerHTML").value = sum;
 }
+function remainder() {
+  let first = document.querySelector("#first").value;
+  let second = document.querySelector("#second").value;
+  let sum = Number(first) % Number(second);
+  document.querySelector("#answerHTML").value = sum;
+}
